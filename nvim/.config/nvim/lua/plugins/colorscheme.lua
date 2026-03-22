1924f38497af7fc972519d422214ddb95e104028
Justin Cordova <justinavodroc@gmail.com>
2026-03-22 09:48:27 -0400

    add rose-pine

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -56,4 +56,53 @@ return {
       }
     end,
   },
+
+  -- Rosé Pine
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    lazy = false,
+    priority = 999,
+    opts = {
+      variant = 'auto',
+      dark_variant = 'main',
+      dim_inactive_windows = false,
+      extend_background_behind_borders = true,
+      enable = {
+        terminal = true,
+        legacy_highlights = true,
+        migrations = true,
+      },
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = false,
+      },
+      groups = {
+        border = 'muted',
+        link = 'iris',
+        panel = 'surface',
+        error = 'love',
+        hint = 'iris',
+        info = 'foam',
+        note = 'pine',
+        todo = 'rose',
+        warn = 'gold',
+        git_add = 'foam',
+        git_change = 'rose',
+        git_delete = 'love',
+        git_dirty = 'rose',
+        git_ignore = 'muted',
+        git_merge = 'iris',
+        git_rename = 'pine',
+        git_stage = 'iris',
+        git_text = 'rose',
+        git_untracked = 'subtle',
+      },
+      highlight_groups = {},
+    },
+    config = function(_, opts)
+      require('rose-pine').setup(opts)
+    end,
+  },
 }
